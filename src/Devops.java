4218c3938b43911ae45aab3487f63f45eec5f7c5
System Administrator <root@United-IRON-MAN.local>
2018-09-20 12:15:02 +0530

    first commit

---
+++ src/Devops.java
@@ -0,0 +1,9 @@
+
+public class Devops {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World welcome to Devops");
+
+	}
+
+}
---
+++ src/DevopsTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+class DevopsTest {
+
+	@Test
+	void testMain() {
+		pass("true,true");
+	}
+
+	private void pass(String string) {
+		System.out.println("Hello World welcome to Devops");
+		
+	}
+
+}
